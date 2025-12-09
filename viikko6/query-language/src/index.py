1c8419c57243751977ff6741cbaf932a6777ecf8
OtsoH <otsoheinamaki@gmail.com>
2025-12-09 21:33:39 +0200

    complete task 5

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All, Or
+from matchers import QueryBuilder
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players.txt"
@@ -8,60 +8,36 @@ def main():
     stats = Statistics(reader)
 
     # Testi 1
-    print("Pelaajat, joilla ei ole vähintään 2 maalia NYR:ssä:")
-    matcher = And(
-        Not(HasAtLeast(2, "goals")),
-        PlaysIn("NYR")
-    )
+    print("Kaikki pelaajat (määrä):")
+    query = QueryBuilder()
+    matcher = query.build()
+    print(len(stats.matches(matcher)))
+
+    print("\n" + "="*50 + "\n")
+
+    # Testi 2
+    print("NYR-pelaajat:")
+    query = QueryBuilder()
+    matcher = query.plays_in("NYR").build()
 
     for player in stats.matches(matcher):
         print(player)
 
     print("\n" + "="*50 + "\n")
 
-    # Testi 2
-    print("Pelaajat, joilla on alle 2 maalia NYR:ssä:")
-    matcher2 = And(
-        HasFewerThan(2, "goals"),
-        PlaysIn("NYR")
-    )
-
-    for player in stats.matches(matcher2):
-        print(player)
-
-    print("\n" + "="*50 + "\n")
-
     # Testi 3
-    print("Kaikkien pelaajien määrä:")
-    filtered_with_all = stats.matches(All())
-    print(len(filtered_with_all))
+    print("NYR-pelaajat, joilla 10-19 maalia:")
+    query = QueryBuilder()
 
-    print("\n" + "="*50 + "\n")
-
-    # Testi 4
-    print("Pelaajat, joilla vähintään 45 maalia tai 70 syöttöä:")
-    matcher3 = Or(
-        HasAtLeast(45, "goals"),
-        HasAtLeast(70, "assists")
+    matcher = (
+        query
+        .plays_in("NYR")
+        .has_at_least(10, "goals")
+        .has_fewer_than(20, "goals")
+        .build()
     )
 
-    for player in stats.matches(matcher3):
-        print(player)
-
-    print("\n" + "="*50 + "\n")
-
-    # Testi 5
-    print("Pelaajat, joilla vähintään 70 pistettä ja pelaavat COL/FLA/BOS:")
-    matcher4 = And(
-        HasAtLeast(70, "points"),
-        Or(
-            PlaysIn("COL"),
-            PlaysIn("FLA"),
-            PlaysIn("BOS")
-        )
-    )
-
-    for player in stats.matches(matcher4):
+    for player in stats.matches(matcher):
         print(player)
 
 if __name__ == "__main__":
